3c896bed637287730424ec1966a68bca050c59df
Cyb3r8 <ben@wagnertb.ch>
2025-07-16 16:19:01 +0200

    .

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class PokeBot
     {
-        public const string Attribution = "https://github.com/hexbyt3/PokeBot";
+        public const string Attribution = "https://github.com/Cyb3r8/PokeBot";
 
         public const string ConfigPath = "config.json";
 

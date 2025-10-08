8df2f1904d58eb5c38530a24e2723b12503e0423
Taku1991 <Takuta1991@outlook.at>
2025-10-08 20:37:46 +0200

    push version

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -12,7 +12,7 @@ public static class PokeBot
 
     public const string ConfigPath = "config.json";
 
-    public const string Version = "v1.1.24";
+    public const string Version = "v1.1.24a";
 
     /// <summary>
     /// Checks if a user can use AutoOT functionality.
